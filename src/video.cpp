d212ecd05ee5fcbdd569a27865b9acd4525e3ef3
ingframin <framin2@gmail.com>
2020-08-25 11:54:23 +0200

    Part 3: Video Subsystem

---
+++ src/video.cpp
@@ -0,0 +1,76 @@
+#include "video.h"
+#include <iostream>
+
+Video::Video(const std::string& title,int width,int height){
+    //Let's start separating the different subsystems initialization
+    if(!SDL_WasInit(SDL_INIT_VIDEO)){
+        SDL_Init(SDL_INIT_VIDEO);
+    }
+    
+    wnd = SDL_CreateWindow(title.c_str(),SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,width,height,SDL_WINDOW_SHOWN);
+    if(wnd == NULL){
+        // In the case that the window could not be made...
+        std::cout<<"Could not create window: "<<SDL_GetError()<<std::endl;
+        exit(-1);
+    }
+    rnd = SDL_CreateRenderer(wnd,-1,SDL_RENDERER_ACCELERATED);
+    if(rnd == NULL){
+        std::cout<<"Could not create renderer: "<<SDL_GetError()<<std::endl;
+        exit(-1);
+    }
+
+}
+
+Video::~Video(){
+    clearTextures();
+    SDL_DestroyRenderer(rnd);
+    SDL_DestroyWindow(wnd);
+    SDL_QuitSubSystem(SDL_INIT_VIDEO);
+}
+
+void Video::clear(){
+    //Set draw color to black
+    SDL_SetRenderDrawColor(rnd,0,0,0,0);
+    //Clear Screen
+    SDL_RenderClear(rnd);
+}
+
+
+void Video::draw(Sprite& sp){
+    SDL_RenderCopy(rnd,textures[sp.id],NULL,&sp.rect);
+
+}
+
+void Video::setDrawColor(uint8_t r, uint8_t g, uint8_t b){
+    SDL_SetRenderDrawColor(rnd,r,g,b,255);
+}
+
+void Video::drawLine(int x0, int y0, int x1, int y1){
+    SDL_RenderDrawLine(rnd,x0,y0,x1,y1);
+}
+
+void Video::flip(){
+    SDL_RenderPresent(rnd);
+}
+
+Sprite Video::loadTexture(SDL_Surface* srf){
+    SDL_Texture* txt = SDL_CreateTextureFromSurface(rnd,srf);
+    
+    Sprite sprt;
+    sprt.rect.x = 0;
+    sprt.rect.y = 0;
+    sprt.rect.w = srf->w;
+    sprt.rect.h = srf->h;
+    sprt.id = textures.size();
+
+    textures.push_back(txt);
+
+    return sprt;
+}
+
+void Video::clearTextures(){
+    for(auto txt:textures){
+        SDL_DestroyTexture(txt);
+    }
+    textures.clear();
+}
---
+++ src/video.h
@@ -0,0 +1,46 @@
+#ifndef VIDEOSUB
+#define VIDEOSUB
+#include <SDL2/SDL.h>
+#include <vector>
+#include <string>
+
+// This is a graphical object.
+// Its position on screen depends on the transformation between 
+// game space and screen space
+struct Sprite{
+    //Drawing area in screen coordinates
+    SDL_Rect rect;
+    //ID of the texture to be drawn
+    uint64_t id;
+};
+
+class Video
+{
+private:
+    SDL_Window* wnd;
+    SDL_Renderer* rnd;
+    std::vector<SDL_Texture*> textures;
+
+public:
+    Video(const std::string& title,int width,int height);
+    ~Video();
+    //Clear screen to black
+    void clear();
+    //Draws a sprite to a specific location on screen.  
+    //We are not going to manage animations and rotations yet
+    void draw(Sprite& sp);
+
+    //Geometry drawing functions
+    void setDrawColor(uint8_t r, uint8_t g, uint8_t b);
+    void drawLine(int x0, int y0, int x1, int y1);
+    
+    //Present on screen the current draw buffer
+    void flip();
+    //loads a sprite in the sprite buffer and returns its position/id. 
+    Sprite loadTexture(SDL_Surface* srf);
+    //removes all texture pointers from the current buffer and destroys them
+    void clearTextures();
+};
+
+
+#endif
